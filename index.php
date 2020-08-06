6aed262f77297de33b055af6dec8f8f7fd8320c1
nishantmendhe <69115076+nishantmendhe@users.noreply.github.com>
2020-08-06 14:17:49 +0530

    Update index.php

--- index.php
+++ index.php
@@ -23,7 +23,7 @@ else
 </head>
 <body>
     <section class="congratulations">
-        <h1>Saurabh Badhel!</h1>
+        <h1>Bhai h apna Saurabh Badhel!</h1>
         <p>We have Successfully done with CICD Pipeline using GITHUB <em>PHP</em> application is now running on your own dedicated environment in the AWS&nbsp;Cloud</p>
         <p>You are running PHP version <?= phpversion() ?></p>
     </section>
